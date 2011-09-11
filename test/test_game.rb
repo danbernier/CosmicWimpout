000841857c7fa52bd14f7fdfbc88ec7a7bc3ce8f
Dan Bernier <danbernier@gmail.com>
2011-09-11 17:23:53 -0400

    Added tests around players choosing whether to stop early, cosmic wimpouts, and scoring on all 5 cubes

--- test/test_game.rb
+++ test/test_game.rb
@@ -6,8 +6,8 @@ require 'cosmic_wimpout'
 describe CosmicWimpout::Game do
 
   before do
-    @tortoise = CosmicWimpout::Player.new "Tortoise"
-    @achilles = CosmicWimpout::Player.new "Achilles"
+    @tortoise = MockPlayer.new "Tortoise"
+    @achilles = MockPlayer.new "Achilles"
     @game = CosmicWimpout::Game.new @tortoise, @achilles
   end
 
@@ -27,9 +27,52 @@ describe CosmicWimpout::Game do
 
   describe "when numbers and symbols are rolled" do
     it "should let the player decide: re-roll the symbol cubes?" do
-      fox_the_dice(5, 10, [:two, 5], [:two, 10], [:three, :four, 10])
-      
-      
+      fox_the_dice(5, 5, [:two, 10], [:two, 10], [:three, :four])
+
+      # Tortoise will get 10 pts first toss, and re-roll.
+      # He'll get 20 more second toss, and stop.
+      @tortoise.rerolls_if do |cubes, turn_points|
+        cubes.size > 1
+      end
+
+      @game.take_turn
+
+      @tortoise.points.must_equal 30
+      @tortoise.roll_decisions[0].must_equal [[:two, :two, :three], 10]
+      @tortoise.roll_decisions[1].must_equal [[:four], 30]
+
+      # Achilles ALWAYS re-rolls. Eventually, he'll lose his points.
+      @achilles.rerolls_if do |cubes, turn_points|
+        true  # Too eager!
+      end
+
+      @game.take_turn
+
+      @achilles.points.must_equal 0
+      @achilles.roll_decisions[0].must_equal [[:two, :two, :three], 10]
+      @achilles.roll_decisions[1].must_equal [[:four], 30]
+    end
+  end
+
+  describe "when only symbols are rolled" do
+    it "should end the turn immediately, with 0 points" do
+      fox_the_dice :two, :three, :six, :two, :four
+
+      @game.take_turn
+
+      @tortoise.points.must_equal 0
+      @tortoise.roll_decisions.size.must_equal 0
+    end
+  end
+
+  describe "when only numbers are rolled" do
+    it "should end the turn immediately, with all the points" do
+      fox_the_dice 5, 5, 10, 10, 10
+
+      @game.take_turn
+
+      @tortoise.points.must_equal 40
+      @tortoise.roll_decisions.size.must_equal 0
     end
   end
 
@@ -53,6 +96,24 @@ describe CosmicWimpout::Game do
     end
     def toss
       @face_up = @fixed_rolls.next
+    end
+  end
+
+  class MockPlayer < CosmicWimpout::Player
+    attr_accessor :roll_decisions
+
+    def initialize name
+      super name
+      @roll_decisions = []
+    end
+
+    def rerolls_if &block
+      @reroll_when = block
+    end
+
+    def roll_again? cubes, turn_points
+      @roll_decisions.push [cubes.map(&:face_up), turn_points]
+      @reroll_when.call cubes, turn_points
     end
   end
 
