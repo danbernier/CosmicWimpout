61ff55de7b7b36f0da2ead3671997c31cac83429
Dan Bernier <danbernier@gmail.com>
2011-09-12 20:19:24 -0400

    Use require_relative, instead of that hoary __FILE__ based thing

--- lib/cosmic_wimpout.rb
+++ lib/cosmic_wimpout.rb
@@ -1,4 +1,4 @@
 # WTF? http://stackoverflow.com/questions/2900370/why-does-ruby-1-9-2-remove-from-load-path-and-whats-the-alternative
-require File.expand_path(File.join(File.dirname(__FILE__), 'cosmic_wimpout/game'))
-require File.expand_path(File.join(File.dirname(__FILE__), 'cosmic_wimpout/player'))
-require File.expand_path(File.join(File.dirname(__FILE__), 'cosmic_wimpout/cube'))
+require_relative 'cosmic_wimpout/game'
+require_relative 'cosmic_wimpout/player'
+require_relative 'cosmic_wimpout/cube'
