fc5cac948f9034bec6c26393ac7c494e91235df5
Dan Bernier <danbernier@gmail.com>
2011-09-18 09:31:25 -0400

    Removed silly, old comment

--- lib/cosmic_wimpout.rb
+++ lib/cosmic_wimpout.rb
@@ -1,4 +1,3 @@
-# WTF? http://stackoverflow.com/questions/2900370/why-does-ruby-1-9-2-remove-from-load-path-and-whats-the-alternative
 require_relative 'cosmic_wimpout/game'
 require_relative 'cosmic_wimpout/player'
 require_relative 'cosmic_wimpout/cube'
