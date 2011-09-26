5ad45e8e52e0f339f6355060eb3777b440a075da
Dan Bernier <danbernier@gmail.com>
2011-09-25 20:35:42 -0400

    Implement the PlayerView#toss_again? and #pick_value_for_sun.

--- lib/cosmic_wimpout/player_view.rb
+++ lib/cosmic_wimpout/player_view.rb
@@ -10,11 +10,24 @@ module CosmicWimpout
     end
     
     def toss_again?(cubes, turn_points)
-      raise 'not done'
+      puts
+      puts "> #{name}, you have #{turn_points} points this turn."
+      puts "> Do you want to toss these cubes?"
+      puts "> #{cubes.sort_by(&:to_s) * ', '}"
+
+      @hl.ask("> Toss 'em? (y, n) ") { |q| q.in = %w[y n] } == 'y'
     end
     
     def pick_value_for_sun(cubes, turn_points)
-      raise 'not done'
+      
+      puts
+      puts "> #{name}, you rolled a Flaming Sun!"
+      puts "> You rolled:"
+      puts "> #{cubes.sort_by(&:to_s) * ', '}"
+
+      @hl.ask("> How do you want to count the sun?  ", Integer) do |q|
+        q.in = [2,3,4,5,6,10]
+      end
     end
   
   end
