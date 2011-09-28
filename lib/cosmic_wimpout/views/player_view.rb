de54e0060a0aab9df9bf349e243ec4fae5b143f6
Dan Bernier <danbernier@gmail.com>
2011-09-28 13:03:41 -0400

    Replace puts with Highline#say

--- lib/cosmic_wimpout/views/player_view.rb
+++ lib/cosmic_wimpout/views/player_view.rb
@@ -11,20 +11,20 @@ module CosmicWimpout
       end
       
       def toss_again?(cubes, turn_points)
-        puts
-        puts "> #{name}, you have #{turn_points} points this turn."
-        puts "> Do you want to toss these cubes?"
-        puts "> #{cubes.sort_by(&:to_s) * ', '}"
+        @hl.say ''
+        @hl.say "> #{name}, you have #{turn_points} points this turn."
+        @hl.say "> Do you want to toss these cubes?"
+        @hl.say "> #{cubes.sort_by(&:to_s) * ', '}"
 
         @hl.ask("> Toss 'em? (y, n) ") { |q| q.in = %w[y n] } == 'y'
       end
       
       def pick_value_for_sun(cubes, turn_points)
         
-        puts
-        puts "> #{name}, you rolled a Flaming Sun!"
-        puts "> You rolled:"
-        puts "> #{cubes.sort_by(&:to_s) * ', '}"
+        @hl.say ''
+        @hl.say "> #{name}, you rolled a Flaming Sun!"
+        @hl.say "> You rolled:"
+        @hl.say "> #{cubes.sort_by(&:to_s) * ', '}"
 
         @hl.ask("> How do you want to count the sun?  ", Integer) do |q|
           q.in = [2,3,4,5,6,10]
